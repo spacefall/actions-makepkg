36cd22f262ccc606a4f470d9479ef82a2bf61a9d
spacefall <lucarichu@outlook.it>
2023-12-21 16:52:20 +0100

    changed * to ==>

--- entrypoint.sh
+++ entrypoint.sh
@@ -42,21 +42,21 @@ echo $'keyserver hkp://keyserver.ubuntu.com:80\nkeyserver-options auto-key-retri
 cd $directory
 
 # Makes a copy of the source directory
-#echo "* Copy PKGBUILD ..."
-#echo "* Copying source directory"
+#echo "==> Copy PKGBUILD ..."
+#echo "==> Copying source directory"
 #rsync -av --exclude=".*" $directory /tmp/pkg/
 #cd /tmp/pkg
 
 # Run commands
 if [[ -n "$command" ]]
 then
-  echo "* Running pre-build commands"
+  echo "==> Running pre-build commands"
   echo "${command}" > /tmp/cmd.sh
   bash /tmp/cmd.sh
 fi 
 
 # Install dependencies using yay
-echo "* Installing dependencies"
+echo "==> Installing dependencies"
 #yay -Syu --noconfirm --ignore filesystem
 if $skipruntimedeps
 then
@@ -68,28 +68,28 @@ fi
 # Import PGP key if available
 if [[ -n "$pgpkey" ]]
 then
-  echo "* Importing PGP key"
+  echo "==> Importing PGP key"
   echo "$pgpkey" | base64 -d | gpg --import -
-  echo "* Initializing lsign-key"
+  echo "==> Initializing lsign-key"
   pacman-key --init
 fi
 
 # Build
-echo "* Building"
+echo "==> Building"
 # If $pgpkey is empty, don't sign the package
 [ -n "$pgpkey" ] && makepkg --sign --nodeps || makepkg --nodeps
 
 # Checking
-echo "* Verifying package"
+echo "==> Verifying package"
 source /etc/makepkg.conf # get PKGEXT
 
-namcap "${pkgname}"*"${PKGEXT}"
-pacman -Qip "${pkgname}"*"${PKGEXT}"
-pacman -Qlp "${pkgname}"*"${PKGEXT}"
+namcap "${pkgname}"==>"${PKGEXT}"
+pacman -Qip "${pkgname}"==>"${PKGEXT}"
+pacman -Qlp "${pkgname}"==>"${PKGEXT}"
 
 # Sanitizing file name for artifact upload
-rename ":" "_" "${pkgname}"*"${PKGEXT}" -a || true
+rename ":" "_" "${pkgname}"==>"${PKGEXT}" -a || true
 
-#echo "* Moving back provided package"
+#echo "==> Moving back provided package"
 #sudo chown $(stat -c '%u:%g' $directory/PKGBUILD) ./*.pkg.tar.*
 #sudo mv ./*.pkg.tar.* $directory
